162fa76aca80e42f9d757edfc1cb0e8340b562ed
Andrei <andrei.hudalla@outlook.com>
2023-02-03 19:00:43 +0300

    Finished composition ui

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,9 @@
 const main = document.querySelector("main")
 const pageTitle = document.querySelector("title")
-const sidebar = document.querySelector("#sidebar")
+const sidebar = document.getElementById("sidebar")
 const primary = document.querySelector(".primary")
-const list = document.querySelector("#list")
-const viewer = document.querySelector("#viewer")
+const list = document.getElementById("list")
+const viewer = document.getElementById("viewer")
 const http = new XMLHttpRequest()
 const nomail = document.querySelector(".no-mail")
 const compose = document.querySelector(".compose")
@@ -12,9 +12,9 @@ const filters = document.querySelector(".filters")
 const filter_options = document.querySelector(".filter-options")
 const filter_button = document.querySelector(".filter-button")
 const filter_options_all = document.querySelectorAll(".filter-options > h3")
-const settings_button = document.querySelector("#settings_button")
-const close_settings_button = document.querySelector("#close-settings")
-const settings = document.querySelector("#settings")
+const settings_button = document.getElementById("settings_button")
+const close_settings_button = document.getElementById("close-settings")
+const settings = document.getElementById("settings")
 const sidebar_buttons = document.querySelectorAll("#folders > button")
 const settings_option_buttons = document.querySelectorAll("#settings .options > button")
 const settings_content = document.querySelector("#settings .content")
@@ -23,6 +23,9 @@ const language_labels = document.querySelectorAll(".languages > p")
 const upload_element = document.getElementById("uploadElement")
 const upload_button = document.getElementById("uploadButton")
 const upload_file_preview = document.getElementById("file-preview")
+const textarea = document.getElementById("textarea")
+const input_preview = document.getElementById("input_preview")
+const compose_buttons = document.querySelectorAll("#composition-buttons > button")
 const all = document.querySelectorAll("*")
 
 var last_exception_tick = 0
@@ -31,6 +34,130 @@ var last_folder = null
 var ItemsRendered = 0
 const Step = 15
 var using_filters = []
+
+function markdown(src) {
+
+    var rx_lt = /</g;
+    var rx_gt = />/g;
+    var rx_space = /\t|\r|\uf8ff/g;
+    var rx_escape = /\\([\\\|`*_{}\[\]()#+\-~])/g;
+    var rx_hr = /^([*\-=_] *){3,}$/gm;
+    var rx_blockquote = /\n *&gt; *([^]*?)(?=(\n|$){2})/g;
+    var rx_list = /\n( *)(?:[*\-+]|((\d+)|([a-z])|[A-Z])[.)]) +([^]*?)(?=(\n|$){2})/g;
+    var rx_listjoin = /<\/(ol|ul)>\n\n<\1>/g;
+    var rx_highlight = /(^|[^A-Za-z\d\\])(([*_])|(~)|(\^)|(--)|(\+\+)|`)(\2?)([^<]*?)\2\8(?!\2)(?=\W|_|$)/g;
+    var rx_code = /\n((```|~~~).*\n?([^]*?)\n?\2|((    .*?\n)+))/g;
+    var rx_link = /((!?)\[(.*?)\]\((.*?)( ".*")?\)|\\([\\`*_{}\[\]()#+\-.!~]))/g;
+    var rx_table = /\n(( *\|.*?\| *\n)+)/g;
+    var rx_thead = /^.*\n( *\|( *\:?-+\:?-+\:? *\|)* *\n|)/;
+    var rx_row = /.*\n/g;
+    var rx_cell = /\||(.*?[^\\])\|/g;
+    var rx_heading = /(?=^|>|\n)([>\s]*?)(#{1,6}) (.*?)( #*)? *(?=\n|$)/g;
+    var rx_para = /(?=^|>|\n)\s*\n+([^<]+?)\n+\s*(?=\n|<|$)/g;
+    var rx_stash = /-\d+\uf8ff/g;
+
+    function replace(rex, fn) {
+        src = src.replace(rex, fn);
+    }
+
+    function element(tag, content) {
+        return '<' + tag + '>' + content + '</' + tag + '>';
+    }
+
+    function blockquote(src) {
+        return src.replace(rx_blockquote, function(all, content) {
+            return element('blockquote', blockquote(highlight(content.replace(/^ *&gt; */gm, ''))));
+        });
+    }
+
+    function list(src) {
+        return src.replace(rx_list, function(all, ind, ol, num, low, content) {
+            var entry = element('li', highlight(content.split(
+                RegExp('\n ?' + ind + '(?:(?:\\d+|[a-zA-Z])[.)]|[*\\-+]) +', 'g')).map(list).join('</li><li>')));
+
+            return '\n' + (ol
+                ? '<ol start="' + (num
+                    ? ol + '">'
+                    : parseInt(ol,36) - 9 + '" style="list-style-type:' + (low ? 'low' : 'upp') + 'er-alpha">') + entry + '</ol>'
+                : element('ul', entry));
+        });
+    }
+
+    function highlight(src) {
+        return src.replace(rx_highlight, function(all, _, p1, emp, sub, sup, small, big, p2, content) {
+            return _ + element(
+                  emp ? (p2 ? 'strong' : 'em')
+                : sub ? (p2 ? 's' : 'sub')
+                : sup ? 'sup'
+                : small ? 'small'
+                : big ? 'big'
+                : 'code',
+                highlight(content));
+        });
+    }
+
+    function unesc(str) {
+        return str.replace(rx_escape, '$1');
+    }
+
+    var stash = [];
+    var si = 0;
+
+    src = '\n' + src + '\n';
+
+    replace(rx_lt, '&lt;');
+    replace(rx_gt, '&gt;');
+    replace(rx_space, '  ');
+
+    // blockquote
+    src = blockquote(src);
+
+    // horizontal rule
+    replace(rx_hr, '<hr/>');
+
+    // list
+    src = list(src);
+    replace(rx_listjoin, '');
+
+    // code
+    replace(rx_code, function(all, p1, p2, p3, p4) {
+        stash[--si] = element('pre', element('code', p3||p4.replace(/^    /gm, '')));
+        return si + '\uf8ff';
+    });
+
+    // link or image
+    replace(rx_link, function(all, p1, p2, p3, p4, p5, p6) {
+        stash[--si] = p4
+            ? p2
+                ? '<img src="' + p4 + '" alt="' + p3 + '"/>'
+                : '<a href="' + p4 + '">' + unesc(highlight(p3)) + '</a>'
+            : p6;
+        return si + '\uf8ff';
+    });
+
+    // table
+    replace(rx_table, function(all, table) {
+        var sep = table.match(rx_thead)[1];
+        return '\n' + element('table',
+            table.replace(rx_row, function(row, ri) {
+                return row == sep ? '' : element('tr', row.replace(rx_cell, function(all, cell, ci) {
+                    return ci ? element(sep && !ri ? 'th' : 'td', unesc(highlight(cell || ''))) : ''
+                }))
+            })
+        )
+    });
+
+    // heading
+    replace(rx_heading, function(all, _, p1, p2) { return _ + element('h' + p1.length, unesc(highlight(p2))) });
+
+    // paragraph
+    replace(rx_para, function(all, content) { return element('p', unesc(highlight(content))) });
+
+    // stash
+    replace(rx_stash, function(all) { return stash[parseInt(all)] });
+
+    return src.trim();
+};
 
 function addClass(element,setClass){
     element.classList.add(setClass)
@@ -891,9 +1018,35 @@ function formatSizeUnits(bytes){
     return bytes;
 }
 
-function createFilePreview(imgSource,sizeInBytes) {
+function createFilePreview(imgSource,sizeInBytes,extention) {
+    const valid_file_extentions = [
+        "css",
+        "doc",
+        "docx",
+        "html",
+        "js",
+        "mp3",
+        "pdf",
+        "svg",
+        "txt",
+        "zip",
+        "mp4"
+    ]
+    const img_file_extentions = [
+        "png",
+        "jpeg",
+        "gif",
+        "jpg",
+        "webm"
+    ]
     const preview_root = createElem("div",upload_file_preview)
-    preview_root.style = "background-image: url("+imgSource+")"
+    if (valid_file_extentions.includes(extention)) {
+        preview_root.style = "background-image: url(media/file_extentions/"+extention+".png)"
+    } else if (img_file_extentions.includes(extention)) {
+        preview_root.style = "background-image: url("+imgSource+")"
+    } else {
+        preview_root.style = "background-image: url(media/file_extentions/unknown.png)"
+    }
     const darken_element = createElem("div",preview_root)
     const size_text = createElem("p",darken_element)
     size_text.innerHTML = sizeInBytes
@@ -904,7 +1057,7 @@ function createFilePreview(imgSource,sizeInBytes) {
         last_exception_tick = tick()
         compose.classList.remove("hidden")
     })
-} // Add text <textarea>
+}
 
 function handleFiles() {
     const fileList = this.files;
@@ -913,9 +1066,31 @@ function handleFiles() {
         const source = URL.createObjectURL(element);
         const fileName = element.name
         const extention = fileName.split('.').pop()
-        createFilePreview(source,formatSizeUnits(element.size))
+        createFilePreview(source,formatSizeUnits(element.size),extention)
     }
 }
+
+function updateOutputPreview() {
+    const output = markdown(textarea.value)
+    input_preview.innerHTML = output
+}
+
+function setupTutorial() {
+    if (lang_current == "ru") {
+        textarea.value = ""
+    } else {
+        textarea.value = ""
+    }
+    updateOutputPreview()
+}
+
+textarea.addEventListener("input",updateOutputPreview)
+
+compose_buttons.forEach((element) => {
+    element.addEventListener("click",() => {
+        compose.classList.add("hidden")
+    })
+})
 
 upload_element.addEventListener("change", handleFiles, false);
 upload_button.addEventListener("click",() => { upload_element.click() })
